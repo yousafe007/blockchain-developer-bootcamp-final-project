97e30c3043c5ff1cf39ec2f1eb3a14a9248ca237
Yousuf Ejazi <yousafe007@icloud.com>
2021-11-28 19:15:55 +0100

    Modifies comments in the smart contract

--- contracts/CovVrfy.sol
+++ contracts/CovVrfy.sol
@@ -8,21 +8,17 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Cov-VRFY
 /// @author Yousuf Ejazi
-/// @notice You can use this contract to save and verify sha256 hashes of documents. The primary use of the contract is intended for verifing and submitting covid certificated on-chain.
+/// @notice You can use this contract to save and verify sha256 hashes of strings. The primary use of the contract is intended for verifing and submitting covid certificates on-chain. It is based on the Proof of Existence exercise from the bootcamp.
+
 /// @dev the contract is developed for a coding bootcamp and not intended to be used for production.
 
 contract CovVrfy is Pausable,Ownable {
 
-  /// @notice Emit the submission of a certain hash to chain
+  /// @notice event for the submission of a certain hash of the certificate to chain
   /// @param certInfo the data that will be submitted and saved to state, representing the certificate's information
   event LogSubmission(string certInfo);
 
-
-
-
-
-
-  /// @notice An array of type byte32 to store the hash of the covid certs
+  /// @notice An array of type byte32 to store the hashes of the covid certs.
   bytes32[] private certs;
 
 
@@ -50,7 +46,7 @@ contract CovVrfy is Pausable,Ownable {
     emit LogSubmission(certInfo);
   }
 
-  /// @notice This function calculates the hash of the passed in string, which is the decoded info from the covid cert
+  /// @notice This function calculates the hash of the passed in string, which is the decoded info from the covid-certificate qr-code
   /// @param certInfo The input to be hashed
   /// @return the calculated hash
 
@@ -65,6 +61,7 @@ contract CovVrfy is Pausable,Ownable {
   /// @notice Checks if the cert has already been saved on chain. Uses an internal function which carries the function
   /// @param certInfo The input to be checked for existence
   /// @return boolean. return true if the info has already been saved on chain and false otherwise
+  /// @dev when implementing the frontend, this function is supposed to be used by the dev for checking the existence of a certain hash in the array
 
   function checkCert(string memory certInfo) 
     public 
@@ -75,10 +72,10 @@ contract CovVrfy is Pausable,Ownable {
     return certExists(hash);
   }
 
-  /// @notice returns true if proof is stored
+  /// @notice returns true if the hash is stored is stored
 
-  /// @param cert The input to be checked for existence
-  /// @return return true if the input exists in the state array and false otherwise
+  /// @param cert The input to be checked for its existence in the state array
+  /// @return returns true if the input exists in the state array and false otherwise
 
   function certExists(bytes32 cert) 
     internal 
@@ -94,7 +91,7 @@ contract CovVrfy is Pausable,Ownable {
   }
 
   /// @notice pauses the contract. 
-  /// @dev refer to openzepplin's implementation of the pause onlyOwner modifier
+  /// @dev refer to openzepplin's implementation of the onlyOwner modifier
   function pause()
     public 
     onlyOwner {
@@ -102,8 +99,8 @@ contract CovVrfy is Pausable,Ownable {
   }
 
 
-  /// @notice unpauses the contract. 
-  /// @dev refer to openzepplin's implementation of the unpause onlyOwner modifier
+  /// @notice unpauses the contract (Only the owner is allowed to call this function). 
+  /// @dev refer to openzepplin's implementation of the onlyOwner modifier.
   function unpause() 
     public 
     onlyOwner {
@@ -111,6 +108,3 @@ contract CovVrfy is Pausable,Ownable {
   }
 
 }
-
-
-// 0x6B29DDD015b66D799B5DA08238E69D7BB59279C6
